f45fc4a2b38acfebd31a2ead4072ad246f73d5f6
shpp-achudinovskyh <andron989@gmail.com>
2016-10-11 23:04:38 +0300

    add data to JSON converter

--- listener.js
+++ listener.js
@@ -40,3 +40,16 @@ users.forEach(user => {
 
   server.listen(user.port);
 });
+
+function dataToJSON(data) {
+    let tempData = data.split(" ");
+    tempData[1] = tempData[1].substr(0, 10) + " " + tempData[1].substr(11,8);
+    let data = {
+        'date':`${tempData[1]}`,
+        'user_id': `${user.host}:${user.port}`,
+        'sender_ip': `${tempData[2]}`,
+        'file_name': `${tempData[3]}`,
+        'message': `${tempData[7]}`
+    };
+    return data;
+}
