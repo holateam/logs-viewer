299e72a0c53bd87a763dfff90cf54d266038ab5b
Alex Belokon <info.tehnokot@gmail.com>
2016-10-13 12:32:51 +0300

    added module logging

--- listener.js
+++ listener.js
@@ -1,7 +1,5 @@
-const mongoose = require('mongoose');
-const db = require('./modules/db');
-const UserLogSchema = require('./modules/userLog');
 const queryDB = require('./modules/queryDB');
+const logging = require('./modules/logging');
 
 let users = [{
   host: "localhost",
@@ -19,7 +17,8 @@ users.forEach(user => {
       // convert data to JSON
 
       // write JSON to DB
-      onDataReceived(data, user);
+    //   onDataReceived(data, user);
+        logging.saveDataMongoDB(data, user);
 
       //socketIO.emit("new_logs_received", data);
       //socket.end();
@@ -28,37 +27,3 @@ users.forEach(user => {
 
   server.listen(user.port);
 });
-
-
-function dataToJSON(data, user) {
-    let tempData = data.toString().split(" ");
-    tempData[1] = tempData[1].substr(0, 10) + " " + tempData[1].substr(11,8);
-    let result = {
-        'date':`${tempData[1]}`,
-        'user_id': `${user.host}:${user.port}`,
-        'sender_ip': `${tempData[2]}`,
-        'file_name': `${tempData[3]}`,
-        'message': `${tempData[7]}`
-    };
-    return result;
-}
-
-function generateEntityFromData (data, user){
-    return  dataToJSON(data, user);
-    // return {
-    //     date: "2016-10-06 23:59:59",
-    //     user_id: `localhost:30000`,
-    //     sender_ip: '127.0.0.1',
-    //     file_name: 'admin.log',
-    //     message: 'Some body text'
-    // };
-}
-
-function onDataSaved(err){
-    if (err) throw err;
-}
-
-function onDataReceived(data, user){
-     let record = new UserLogSchema(generateEntityFromData (data, user));
-     record.save(onDataSaved);
-}
